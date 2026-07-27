5e7b9f852e716a5d3764d41f761dcdb63b703119
nanafrema957-web <nanafrema957@gmail.com>
2026-07-27 22:45:28 +0000

    Completed Assignment 1

--- assignment_01_prime_checker.py
+++ assignment_01_prime_checker.py
@@ -35,3 +35,20 @@
 # YOUR CODE BELOW — remove the # symbols from the scaffold and fill it in
 # =============================================================================
 
+number=int(input("enter a number:"))
+
+if number<=1:
+    print(number, "is not a prime number")
+else:
+    is_prime=True
+
+    for i in range(2,number):
+        if number % i==0: 
+            is_prime=False
+            break
+    if is_prime:
+        print(number, "is a prime number")
+    else:
+        print(number, "is not a prime number")
+
+
